0744ce84eecd68313f826c8cc5a2d0d8c01bd605
Daniel Nogueira <daninvasc2@gmail.com>
2026-04-27 10:02:04 -0300

    Added day_price and night_price to court entity

--- src/migrations/1745472000000-SeedDefaultCourts.ts
+++ src/migrations/1745472000000-SeedDefaultCourts.ts
@@ -5,11 +5,11 @@ export class SeedDefaultCourts1745472000000 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      INSERT INTO \`courts\` (\`name\`, \`active\`) VALUES
-      ('Quadra 1', 1),
-      ('Quadra 2', 1),
-      ('Quadra 3', 1),
-      ('Quadra 4', 1)
+      INSERT INTO \`courts\` (\`name\`, \`active\`, \`day_price\`, \`night_price\`) VALUES
+      ('Quadra 1', 1, 90, 90),
+      ('Quadra 2', 1, 70, 80),
+      ('Quadra 3', 1, 70, 80),
+      ('Quadra 4', 1, 70, 80)
     `);
   }
 
